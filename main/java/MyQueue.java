cba461c42875b64d76dc5c1048722daee2bed64e
Atharvatathe <atharva.tathe55@gmail.com>
2021-04-25 21:04:23 +0530

    Added: Create Queue using Linked list

---
+++ main/java/MyQueue.java
@@ -0,0 +1,18 @@
+public class MyQueue{
+
+    private final MyLinkdList myLinkedList;
+    public MyQueue(){
+        this.myLinkedList = new MyLinkdList();
+    }
+
+    public void enqueue(INode myNode){
+
+        myLinkedList.append(myNode);
+    }
+
+    public void printQueue(){
+
+        myLinkedList.printMyNodes();
+    }
+
+}
---
+++ test/java/MyQueueTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class MyQueueTest {
+
+    @Test
+    public void given3NumberWhenAddedtoQueueShouldHaveLastAddedNode(){
+        MyQueue myQueue = new MyQueue();
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+        myQueue.printQueue();
+
+    }
+
+
+}

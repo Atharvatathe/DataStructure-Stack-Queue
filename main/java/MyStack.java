60cdd40042dae30aa98c5fc9798419d52017fc18
Atharvatathe <atharva.tathe55@gmail.com>
2021-04-25 17:16:47 +0530

    Added: popped out element from Stack

--- main/java/MyStack.java
+++ main/java/MyStack.java
@@ -19,6 +19,10 @@ public class MyStack {
         return myLinkedList.head;
     }
 
+
+    public INode pop() {
+        return myLinkedList.pop();
+    }
 }
 
 
--- test/java/MyStackTest.java
+++ test/java/MyStackTest.java
@@ -18,4 +18,19 @@ public class MyStackTest {
 
     }
 
-}
+    @Test
+    public void given3NumberWhenPoppedOuttoStackShouldMatchLastAddedNode() {
+        MyStack myStack = new MyStack();
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        myStack.push(myFirstNode);
+        myStack.push(mySecondNode);
+        myStack.push(myThirdNode);
+        INode pop = myStack.pop();
+        myStack.printStack();
+        Assertions.assertEquals(myThirdNode, pop);
+    }
+
+
+    }
